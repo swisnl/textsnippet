feae3de46e0531c3cb5f49d546e8d9959b04b62b
Jan Barášek <janbarasek@gmail.com>
2020-04-19 19:37:33 +0200

    Fix typehints

--- src/TextSnippet.php
+++ src/TextSnippet.php
@@ -6,14 +6,19 @@ namespace Swis;
 
 class TextSnippet
 {
+	/** @var string[] */
 	protected $specialChars = ['Â', 'Ã', 'Ä', 'À', 'Á', 'Å', 'Æ', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ð', 'Ñ', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ø', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'Þ', 'ß', 'à', 'á', 'â', 'ã', 'ä', 'å', 'æ', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ð', 'ñ', 'ò', 'ó', 'ô', 'õ', 'ö', 'ø', 'ù', 'ú', 'û', 'ü', 'ý', 'þ', 'ÿ', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
+	/** @var string[] */
 	protected $specialReplaces = ['A', 'A', 'A', 'A', 'A', 'A', 'E', 'C', 'E', 'E', 'E', 'E', 'I', 'I', 'I', 'I', 'D', 'N', 'O', 'O', 'O', 'O', 'O', 'O', 'U', 'U', 'U', 'U', 'Y', 'P', 'B', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'o', 'n', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'b', 'y', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
+	/** @var string */
 	protected $highlightTemplate = '<span class="highlighted">%word%</span>';
 
+	/** @var int */
 	protected $minWords = 30;
 
+	/** @var int */
 	protected $maxWords = 100;
 
 
@@ -21,7 +26,7 @@ class TextSnippet
 	 * Break a text into sentences
 	 *
 	 * @param string $text
-	 * @return array
+	 * @return string[]
 	 */
 	public function breakIntoSentences(string $text): array
 	{
@@ -33,10 +38,13 @@ class TextSnippet
 	 * Set the minimum number of words, returned in the snippet
 	 *
 	 * @param int $minWords
+	 * @return TextSnippet
 	 */
-	public function setMinWords(int $minWords)
+	public function setMinWords(int $minWords): self
 	{
 		$this->minWords = $minWords;
+
+		return $this;
 	}
 
 
@@ -44,10 +52,13 @@ class TextSnippet
 	 * Set the maximum number of words, returned in the snippet
 	 *
 	 * @param int $maxWords
+	 * @return TextSnippet
 	 */
-	public function setMaxWords(int $maxWords)
+	public function setMaxWords(int $maxWords): self
 	{
-		$this->maxWords = (int) $maxWords;
+		$this->maxWords = $maxWords;
+
+		return $this;
 	}
 
 
@@ -55,14 +66,17 @@ class TextSnippet
 	 * Set the template for the highlighting, for example '<em>%word%</em>'
 	 *
 	 * @param string $template
+	 * @return TextSnippet
 	 * @throws \RuntimeException
 	 */
-	public function setHighlightTemplate(string $template)
+	public function setHighlightTemplate(string $template): self
 	{
 		if (strpos($template, '%word%') === false) {
 			throw new \RuntimeException('HighlightTemplate should contain "%word%"');
 		}
 		$this->highlightTemplate = $template;
+
+		return $this;
 	}
 
 
@@ -81,9 +95,9 @@ class TextSnippet
 		foreach ($queryWords as $word) {
 			foreach ($snippetWords as $snippetWord) {
 				// case-insensitive matching. accent-insensitive matching
-				if (strtolower(str_replace($this->specialChars, $this->specialReplaces, $word)) ==
+				if (strtolower(str_replace($this->specialChars, $this->specialReplaces, $word)) ===
 					strtolower(str_replace($this->specialChars, $this->specialReplaces, $snippetWord))) {
-					$replaces['/\b' . preg_quote($snippetWord) . '\b/'] = str_replace('%word%', $snippetWord, $this->highlightTemplate);
+					$replaces['/\b' . preg_quote($snippetWord, '/') . '\b/'] = str_replace('%word%', $snippetWord, $this->highlightTemplate);
 				}
 			}
 		}
@@ -112,13 +126,13 @@ class TextSnippet
 		foreach ($matchedSentences as $key => $sentence) {
 			$wordCounter += str_word_count($sentence, 0, implode('', $this->specialChars));
 			if ($wordCounter < $this->maxWords || $result === '') {
-				if ($key != $lastKey + 1) {
+				if ((int) $key !== $lastKey + 1) {
 					// if this sentence is not the next sentence, add ' ... '
 					$result .= ' ...';
 				}
 				$result .= ' ' . $sentence;
 			}
-			$lastKey = $key;
+			$lastKey = (int) $key;
 		}
 
 		// Matched text is smaller than [minWords]. Try to add next sentences
@@ -150,8 +164,8 @@ class TextSnippet
 	 * Keys are the original sentence positions
 	 *
 	 * @param string $query
-	 * @param array $sentences
-	 * @return array
+	 * @param string[] $sentences
+	 * @return string[]
 	 */
 	protected function getMatchedSentences(string $query, array $sentences): array
 	{
